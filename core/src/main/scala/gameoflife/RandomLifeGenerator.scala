a6c2de9dc5d2f14bd993e2b4385ccf5b9ab318fc
wobu <buchnerw@googlemail.com>
2015-03-26 18:48:28 +0100

    fixed bug that 1 line more cells were created than place was available

--- core/src/main/scala/gameoflife/RandomLifeGenerator.scala
+++ core/src/main/scala/gameoflife/RandomLifeGenerator.scala
@@ -5,8 +5,8 @@ import gameoflife.Universe.LivingCell
 object RandomLifeGenerator {
   def apply(dimensions: (X, Y)): Seq[LivingCell] = {
     val randomLocations = for {
-      x <- 0 to dimensions._1
-      y <- 0 to dimensions._2
+      x <- 0 until dimensions._1
+      y <- 0 until dimensions._2
       if math.random < 0.5
     } yield (x, y)
 
